93ac8e1e4ec9f74cb6aeb06e14caf4c80561b07f
velislav088 <veli_don@abv.bg>
2024-09-08 00:18:17 +0300

    Initial commit

---
+++ SecureWebSite.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecureWebSite.Server.Models;
+
+namespace SecureWebSite.Server.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-N2P1BBN\SQLEXPRESS;Database=FMA_TEST2;Integrated Security=True;TrustServerCertificate=True;");
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
