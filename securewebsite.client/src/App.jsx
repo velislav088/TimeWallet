a6dd7ed00d28efd1c2ad323ec8b6437b44fa877d
velislav088 <veli_don@abv.bg>
2024-09-20 00:18:27 +0300

    Reformatted code

--- securewebsite.client/src/App.jsx
+++ securewebsite.client/src/App.jsx
@@ -1,95 +1,95 @@
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Link,
-} from "react-router-dom";
-import ProtectedRoutes from "./ProtectedRoutes";
-import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Link,
+} from "react-router-dom"
+import ProtectedRoutes from "./ProtectedRoutes"
+import Home from "./pages/Home"
+import Admin from "./pages/Admin"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="*"
-        element={
-          <div>
-            <header>
-              <h1>Not Found</h1>
-            </header>
-            <p>
-              <a href="/">Back to Home</a>
-            </p>
-          </div>
-        }
-      />
-    </Route>
-  )
-);
+	createRoutesFromElements(
+		<Route path="/">
+			<Route element={<ProtectedRoutes />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/admin" element={<Admin />} />
+			</Route>
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route
+				path="*"
+				element={
+					<div>
+						<header>
+							<h1>Not Found</h1>
+						</header>
+						<p>
+							<a href="/">Back to Home</a>
+						</p>
+					</div>
+				}
+			/>
+		</Route>
+	)
+)
 function App() {
-  const isLogged = localStorage.getItem("user");
-  const logout = async () => {
-    const response = await fetch("/api/securewebsite/logout", {
-      method: "GET",
-      credentials: "include",
-    });
+	const isLogged = localStorage.getItem("user")
+	const logout = async () => {
+		const response = await fetch("/api/securewebsite/logout", {
+			method: "GET",
+			credentials: "include",
+		})
 
-    const data = await response.json();
-    if (response.ok) {
-      localStorage.removeItem("user");
+		const data = await response.json()
+		if (response.ok) {
+			localStorage.removeItem("user")
 
-      alert(data.message);
+			alert(data.message)
 
-      document.location = "/login";
-    } else {
-      console.log("could not logout: ", response);
-    }
-  };
-  return (
-    <div>
-      <div>
-        {isLogged ? (
-          <span className="navbar">
-            <ul className="navbar-left">
-              <a className="button" href="/">
-                Home
-              </a>
-            </ul>
-            <ul>
-              <a className="button" href="/admin">
-                Admin
-              </a>
-              <a className="button" onClick={logout}>
-                Log Out
-              </a>  
-            </ul>
-          </span>
-        ) : (
-          <span className="navbar">
-            <ul>
-              <a className="button" href="/login">
-                Login
-              </a>
-              <a className="button" href="/register">
-                Register
-              </a>
-            </ul>
-          </span>
-        )}
-      </div>
-      <RouterProvider router={router} />
-    </div>
-  );
+			document.location = "/login"
+		} else {
+			console.log("could not logout: ", response)
+		}
+	}
+	return (
+		<div>
+			<div>
+				{isLogged ? (
+					<span className="navbar">
+						<ul className="navbar-left">
+							<a className="button" href="/">
+								Home
+							</a>
+						</ul>
+						<ul>
+							<a className="button" href="/admin">
+								Admin
+							</a>
+							<a className="button" onClick={logout}>
+								Log Out
+							</a>
+						</ul>
+					</span>
+				) : (
+					<span className="navbar">
+						<ul>
+							<a className="button" href="/login">
+								Login
+							</a>
+							<a className="button" href="/register">
+								Register
+							</a>
+						</ul>
+					</span>
+				)}
+			</div>
+			<RouterProvider router={router} />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- securewebsite.client/src/pages/Register.jsx
+++ securewebsite.client/src/pages/Register.jsx
@@ -1,103 +1,110 @@
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function Register() {
-  document.title = "Register";
+	document.title = "Register"
 
-  // dont ask an already registered user to register over and over again
-  useEffect(() => {
-    const user = localStorage.getItem("user");
-    if (user) {
-      document.location = "/";
-    }
-  }, []);
+	// dont ask an already registered user to register over and over again
+	useEffect(() => {
+		const user = localStorage.getItem("user")
+		if (user) {
+			document.location = "/"
+		}
+	}, [])
 
-  return (
-    <div className="auth-container">
-      <div className="background">
-        <div className="shape"></div>
-        <div className="shape"></div>
-      </div>
-      <form action="#" className="register" onSubmit={registerHandler}>
-        <p className="message"></p>
-        <h3>Register Here</h3>
-        <label htmlFor="name">Name</label>
-        <input type="text" placeholder="Name" name="Name" id="name" required />
+	return (
+		<div className="auth-container">
+			<div className="background">
+				<div className="shape"></div>
+				<div className="shape"></div>
+			</div>
+			<form action="#" className="register" onSubmit={registerHandler}>
+				<p className="message"></p>
+				<h3>Register Here</h3>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					placeholder="Name"
+					name="Name"
+					id="name"
+					required
+				/>
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          placeholder="Email"
-          name="Email"
-          id="email"
-          required
-        />
+				<label htmlFor="email">Email</label>
+				<input
+					type="email"
+					placeholder="Email"
+					name="Email"
+					id="email"
+					required
+				/>
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          name="PasswordHash"
-          id="password"
-          required
-        />
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					placeholder="Password"
+					name="PasswordHash"
+					id="password"
+					required
+				/>
 
-        <input type="submit" value="Register" className="button" />
+				<input type="submit" value="Register" className="button" />
 
-        <div className="logout-redirect">
-          <span>Or </span>
-          <Link to="/login">Login</Link>
-        </div>
-      </form>
-    </div>
-  );
-  async function registerHandler(e) {
-    e.preventDefault();
-    const form_ = e.target,
-      submitter = document.querySelector("input.login");
+				<div className="logout-redirect">
+					<span>Or </span>
+					<Link to="/login">Login</Link>
+				</div>
+			</form>
+		</div>
+	)
+	async function registerHandler(e) {
+		e.preventDefault()
+		const form_ = e.target,
+			submitter = document.querySelector("input.login")
 
-    const formData = new FormData(form_, submitter),
-      dataToSend = {};
+		const formData = new FormData(form_, submitter),
+			dataToSend = {}
 
-    for (const [key, value] of formData) {
-      dataToSend[key] = value;
-    }
+		for (const [key, value] of formData) {
+			dataToSend[key] = value
+		}
 
-    // create username
-    const newUserName = dataToSend.Name.trim().split(" ");
-    dataToSend.UserName = newUserName.join("");
+		// create username
+		const newUserName = dataToSend.Name.trim().split(" ")
+		dataToSend.UserName = newUserName.join("")
 
-    const response = await fetch("api/securewebsite/register", {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(dataToSend),
-      headers: {
-        "content-type": "Application/json",
-        Accept: "application/json",
-      },
-    });
+		const response = await fetch("api/securewebsite/register", {
+			method: "POST",
+			credentials: "include",
+			body: JSON.stringify(dataToSend),
+			headers: {
+				"content-type": "Application/json",
+				Accept: "application/json",
+			},
+		})
 
-    const data = await response.json();
+		const data = await response.json()
 
-    if (response.ok) {
-      document.location = "/login";
-    }
+		if (response.ok) {
+			document.location = "/login"
+		}
 
-    const messageEl = document.querySelector(".message");
-    if (data.message) {
-      messageEl.innerHTML = data.message;
-    } else {
-      let errorMessages = "<div>Attention please:</div><div class='normal'>";
-      data.errors.forEach((error) => {
-        errorMessages += error.description + " ";
-      });
+		const messageEl = document.querySelector(".message")
+		if (data.message) {
+			messageEl.innerHTML = data.message
+		} else {
+			let errorMessages =
+				"<div>Attention please:</div><div class='normal'>"
+			data.errors.forEach((error) => {
+				errorMessages += error.description + " "
+			})
 
-      errorMessages += "</div>";
-      messageEl.innerHTML = errorMessages;
-    }
+			errorMessages += "</div>"
+			messageEl.innerHTML = errorMessages
+		}
 
-    console.log("login error: ", data);
-  }
+		console.log("login error: ", data)
+	}
 }
 
-export default Register;
+export default Register
