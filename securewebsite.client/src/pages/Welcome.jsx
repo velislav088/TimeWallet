349f5b911ced5d754cf2daaba926c76604edd757
velislav088 <veli_don@abv.bg>
2024-11-14 22:32:55 +0200

    Updated footer

--- securewebsite.client/src/pages/Welcome.jsx
+++ securewebsite.client/src/pages/Welcome.jsx
@@ -4,25 +4,40 @@ import welcomeImage from "/src/assets/welcome-image.jpg"
 
 const Welcome = () => {
 	return (
-		<section>
-			<div className="first-section-text">
-				<h1>
-					Save time with <span className="accent">TimeWallet.</span>
-				</h1>
-				<h3>
-					Simplify your finances, budgeting, spending, savings & more
-					with real-time tracking, budget insights,
-					<b className="accent"> and more.</b>
-				</h3>
-				<div className="button-box">
-					<a className="button">Get Started</a>
-					<a className="button button-transparent" href="/register">
-						How It Works
-					</a>
+		<div>
+			<section>
+				<div className="first-section-text">
+					<h1>
+						Save time with{" "}
+						<span className="accent">TimeWallet.</span>
+					</h1>
+					<h3>
+						Simplify your finances, budgeting, spending, savings &
+						more with real-time tracking, budget insights,
+						<b className="accent"> and more.</b>
+					</h3>
+					<div className="button-box">
+						<a className="button">Get Started</a>
+						<a
+							className="button button-transparent"
+							href="/register"
+						>
+							How It Works
+						</a>
+					</div>
 				</div>
-			</div>
-			<img className="welcome-image" src={welcomeImage} alt></img>
-		</section>
+				<img className="welcome-image" src={welcomeImage} alt></img>
+			</section>
+			<footer>
+				<div class="footer-content">
+					<p>© 2024 TimeWallet. All rights reserved.</p>
+					<p>
+						Designed by: <span className="accent">Velislav Donchev</span> |{" "}
+						<span className="accent">Mert Elsenev</span>
+					</p>
+				</div>
+			</footer>
+		</div>
 	)
 }
 
