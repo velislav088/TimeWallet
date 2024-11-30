57965a5b5aa63c11d98f4973e1d47b1b22eb432a
Mert1026 <mertelsenev10@gmail.com>
2024-11-30 02:41:16 +0200

    Final publish!

--- TimeWallet.Server/Data/ApplicationDbContext.cs
+++ TimeWallet.Server/Data/ApplicationDbContext.cs
@@ -21,8 +21,8 @@ namespace TimeWallet.Server.Data
             base.OnConfiguring(optionsBuilder);
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=TrueWalletDB;Trusted_Connection=True;TrustServerCertificate=True;");
-                //optionsBuilder.UseSqlServer("Server=tcp:time-wallet-server.database.windows.net,1433;Initial Catalog=TimeWalletDB;Persist Security Info=False;User ID=MertElsenev;Password=m1e2r3t4!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                //optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=TrueWalletDB;Trusted_Connection=True;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:time-wallet-server.database.windows.net,1433;Initial Catalog=TimeWalletDB;Persist Security Info=False;User ID=MertElsenev;Password=m1e2r3t4!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
