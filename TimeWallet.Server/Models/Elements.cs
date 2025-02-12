833d6beacc032c0351c825c631384d0bb6df90fa
Mert1026 <mertelsenev10@gmail.com>
2025-02-12 18:28:18 +0200

    Tested something,

--- TimeWallet.Server/Models/Elements.cs
+++ TimeWallet.Server/Models/Elements.cs
@@ -10,14 +10,14 @@ namespace TimeWallet.Server.Models
         [Required]
         public Guid id { get; set; }
 
+        [Required]
+        [MaxLength(Common.Common.UserName_Name_Length)]
+        public string name { get; set; }
+
         [ForeignKey(nameof(budgets))]
         [Required]
         public Guid budgetId { get; set; }
         public Budgets budgets { get; set; }
-
-        [Required]
-        [MaxLength(Common.Common.UserName_Name_Length)]
-        public string name { get; set; }
 
         [Required]
         [Range(Common.Common.Moneys_Min_Length, Common.Common.Moneys_Max_Length)]
